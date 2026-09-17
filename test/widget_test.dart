cf6faf865a775dbe1773374bdabf63dbec64bb3a
Edward Summitt <eddiesummitt1645@gmail.com>
2026-09-17 16:08:00 -0500

    few more tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -59,4 +59,67 @@ void main() {
       'Error',
     );
   });
+
+  testWidgets('calculates subtraction', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    await tester.tap(find.text('9'));
+    await tester.tap(find.text('-'));
+    await tester.tap(find.text('4'));
+    await tester.tap(find.text('='));
+    await tester.pump();
+
+    expect(
+      tester.widget<Text>(find.byKey(const Key('display'))).data,
+      '5',
+    );
+  });
+
+  testWidgets('calculates multiplication', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    await tester.tap(find.text('6'));
+    await tester.tap(find.text('x'));
+    await tester.tap(find.text('7'));
+    await tester.tap(find.text('='));
+    await tester.pump();
+
+    expect(
+      tester.widget<Text>(find.byKey(const Key('display'))).data,
+      '42',
+    );
+  });
+
+  testWidgets('calculates decimal values', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    await tester.tap(find.text('1'));
+    await tester.tap(find.text('.'));
+    await tester.tap(find.text('5'));
+    await tester.tap(find.text('+'));
+    await tester.tap(find.text('2'));
+    await tester.tap(find.text('.'));
+    await tester.tap(find.text('5'));
+    await tester.tap(find.text('='));
+    await tester.pump();
+
+    expect(
+      tester.widget<Text>(find.byKey(const Key('display'))).data,
+      '4',
+    );
+  });
+
+  testWidgets('converts a number to a percent', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    await tester.tap(find.text('5'));
+    await tester.tap(find.widgetWithText(TextButton, '0'));
+    await tester.tap(find.text('%'));
+    await tester.pump();
+
+    expect(
+      tester.widget<Text>(find.byKey(const Key('display'))).data,
+      '0.5',
+    );
+  });
 }
